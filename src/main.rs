46d0e53674cbf166e2f163f0291ff12d6787d556
Vinícius Bispo <bispodevacct@gmail.com>
2024-06-23 15:17:15 -0300

    feat: Add the structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct Certifier {
+    name: String,
+    office: String
+}
+
+struct Member {
+    name: String,
+    cpf: String,
+    course: String
+}
+
+struct ShortEvent {
+    name: String,
+    date: String,
+    hours: String
+}
+
+struct LongEvent {
+    name: String,
+    i_date: String,
+    f_date: String
+}
+
 fn main() {
     println!("Hello, world!");
 }
